51598d7e0f3b0a656134aa4a6a8ae31b0b2d5203
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2022-06-24 20:58:26 +0300

    Update Conditional Statements in C.c

--- Conditionals and Loops/Conditional Statements in C.c
+++ Conditionals and Loops/Conditional Statements in C.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    
+    if (num <= 9) 
+    {
+        if (num == 1) 
+        { printf("%s","one"); }
+      
+        else if (num == 2) 
+        { printf("%s","two"); }
+      
+        else if (num == 3) 
+        { printf("%s","three"); }
+      
+        else if (num == 4) 
+        { printf("%s","four"); }   
+      
+        else if (num == 5) 
+        { printf("%s","five"); }         
+      
+         else if (num == 6) 
+         { printf("%s","six"); }           
+      
+        else if (num == 7) 
+        { printf("%s","seven"); }    
+      
+        else if (num == 8) 
+        { printf("%s","eight"); }  
+      
+        else 
+        { printf("%s","nine"); }    
+    }
+  
+    else
+        printf("%s","Greater than 9");
+
+    return 0;
+}
 
