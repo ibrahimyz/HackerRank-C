efb5f6d436bf20f6045c75a3cf9fbea769f9e41d
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2022-06-24 21:02:00 +0300

    Update Sum of Digits of a Five Digit Number.c

--- Conditionals and Loops/Sum of Digits of a Five Digit Number.c
+++ Conditionals and Loops/Sum of Digits of a Five Digit Number.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() 
+{
+    int num, total=0;
+    scanf("%d", &num);
+   
+   for(int i=0; i<5; i++) {
+       int digit = num % 10;
+       num /= 10;
+       total += digit;
+    }
+  
+    printf("%d",total);
+    return 0;
+}
