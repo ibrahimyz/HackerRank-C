1738e4a7ed5bf8c9bb9d9e90fd23f0949f2f542e
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2022-06-24 22:45:41 +0300

    Update Bitwise Operators.c

--- Conditionals and Loops/Bitwise Operators.c
+++ Conditionals and Loops/Bitwise Operators.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+void calculate_the_maximum(int n, int k) 
+{
+    int maxAND=0, maxOR=0, maxXOR=0, and, or, xor;
+  
+    for (int a=1; a<=k; a++) {
+        for (int b=a+1; b<=n; b++) {
+   
+            and=a&b;
+            or=a|b;
+            xor=a^b;
+            
+            if (and>maxAND && and<k)
+                maxAND = a&b;
+            if (or>maxOR && or<k)
+                maxOR = a|b;
+            if (xor>maxXOR && xor<k)
+                maxXOR = a^b;  
+      }
+  }
+  printf("%d\n%d\n%d",maxAND,maxOR,maxXOR);
+  
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+    return 0;
+}
