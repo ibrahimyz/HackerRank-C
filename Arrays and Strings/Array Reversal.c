2ac2fe18b77c37f820f2a9f2fde32d951bd31d79
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2022-06-24 23:33:41 +0300

    Create Array Reversal.c

---
+++ Arrays and Strings/Array Reversal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr;
+    scanf("%d", &num);
+    
+    arr = (int*) malloc(num * sizeof(int));
+    
+    for(int i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    for(int i = num-1; 0 <= i; i--) {
+        printf("%d ",*(arr + i));
+    }
+        
+    return 0;
+}
