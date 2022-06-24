d3b250ffba4642c1f4e51114a0f7478298cd7657
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2022-06-24 20:42:12 +0300

    Update Playing With Characters.c

--- Introduction/Playing With Characters.c
+++ Introduction/Playing With Characters.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() 
+{
+    char ch;
+    char s[100];
+    char sen[100];
+    
+    scanf("%c",&ch);
+    scanf("%s",&s);
+    scanf(" %[^\n]%*c", sen);
+    
+    printf("%c\n%s\n%s",ch,s,sen);
+    
+    return 0;
+}
